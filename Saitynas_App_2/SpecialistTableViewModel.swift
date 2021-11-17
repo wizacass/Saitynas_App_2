2ca9ab8c27fe42ecb73670695d156cba292911d5
Vytenis Petrauskas <visak.pet0@gmail.com>
2021-11-17 14:13:22 +0200

    Retrieve specialists from the API

--- Saitynas_App_2/SpecialistTableViewModel.swift
+++ Saitynas_App_2/SpecialistTableViewModel.swift
@@ -1,28 +1,38 @@
 import Foundation
 
 class SpecialistTableViewModel {
-
+    
     private var observers: [DataSourceObserverDelegate?] = []
-
+    
     var count: Int {
         return specialists.count
     }
-
+    
     private var communicator: Communicator
     private var specialists: [Specialist] = []
-
+    
     init(_ communicator: Communicator) {
         self.communicator = communicator
         loadSpecialists()
     }
-
+    
     private func loadSpecialists() {
-        specialists.append(Specialist(id: 1, firstName: "John", lastName: "Smith", speciality: "Doctor", address: nil))
-        specialists.append(Specialist(id: 2, firstName: "Good", lastName: "Doktor", speciality: "GP", address: nil))
+        communicator.getSpecialists(onSuccess: handleSpecialistsReceived) { error in
+            print("Specialists error received!")
+            print(error?.title)
+            print(error?.details ?? "")
+        }
+        
+    }
+    
+    private func handleSpecialistsReceived(_ dto: SpecialistsDTO?) {
+        guard let data = dto?.data else { return }
+        
+        specialists = data
         
         observers.forEach { $0?.onDataSourceUpdated() }
     }
-
+    
     func getSpecialist(at index: Int) -> Specialist {
         return specialists[index]
     }
@@ -34,7 +44,7 @@ extension SpecialistTableViewModel: Observable {
             observers.append(observer)
         }
     }
-
+    
     func unsubscribe(_ observer: ObserverDelegate) {
         observers = observers.filter { $0?.observerId != observer.observerId }
     }
