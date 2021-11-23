d871c52ee75fdd3c4d1aac70af6c78e138e3dd6b
Vytenis Petrauskas <visak.pet0@gmail.com>
2021-11-23 15:18:15 +0200

    Add custom Swiftlint rule for MARK comments

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -10,8 +10,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    // MARK: UISceneSession Lifecycle
-    
+    // MARK: - UISceneSession Lifecycle
+
     func application(
         _ application: UIApplication,
         configurationForConnecting connectingSceneSession: UISceneSession,
