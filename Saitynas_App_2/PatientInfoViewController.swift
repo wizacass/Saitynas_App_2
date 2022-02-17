69cd60e6852c6fa3f68d17ef602754b115d1cf2b
Vytenis Petrauskas <visak.pet0@gmail.com>
2022-02-17 11:15:42 +0200

    Implement routing between access and patient info view

--- Saitynas_App_2/PatientInfoViewController.swift
+++ Saitynas_App_2/PatientInfoViewController.swift
@@ -4,7 +4,67 @@ class PatientInfoViewController: AccessControllerBase {
 
     @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
 
+    private var authenticationManager: AuthenticationManager?
+    private var communicator: Communicator?
+
+    private let id = UUID()
+
     override func viewDidLoad() {
         super.viewDidLoad(bottomConstraint)
+
+        initialize()
+    }
+
+    private func initialize() {
+        let c = DIContainer.shared
+
+        authenticationManager = c.authenticationManager
+        communicator = c.communicator
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        authenticationManager?.subscribe(self)
+
+        super.viewWillAppear(animated)
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        authenticationManager?.unsubscribe(self)
+
+        super.viewDidDisappear(animated)
+    }
+
+    @IBAction func submitButtonPressed(_ sender: UIButton) {
+
+    }
+
+    @IBAction func laterButtonPressed(_ sender: UIButton) {
+        authenticationManager?.logout()
+    }
+}
+
+extension PatientInfoViewController: StateObserverDelegate {
+    func onLogin(_ user: User?) { }
+
+    func onLogout() {
+        let hasController = hasControllerInStack
+        navigationController?.popViewController(animated: true)
+
+        if hasController { return }
+        if let viewController = storyboard?.instantiateViewController(.authenticationViewController) {
+            navigationController?.pushViewController(viewController, animated: true)
+        }
+    }
+
+    private var hasControllerInStack: Bool {
+        guard let count = navigationController?.viewControllers.count else {
+            return false
+        }
+
+        return count > 1
+    }
+
+    var observerId: UUID {
+        return id
     }
 }
