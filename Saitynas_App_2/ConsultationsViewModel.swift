622dd424dbbe51888c9950b0537713936ff96020
Vytenis Petrauskas <visak.pet0@gmail.com>
2022-03-15 18:28:46 +0200

    Create consultation view

---
+++ Saitynas_App_2/ConsultationsViewModel.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+class ConsultationsViewModel {
+
+    var selectedSpecialityIndex = 0
+    var speciality: [EnumDTO] = []
+
+    private var observers: [DataSourceObserverDelegate?] = []
+
+    private var communicator: Communicator
+
+    init(_ communicator: Communicator) {
+        self.communicator = communicator
+    }
+
+    func loadRoles() {
+        communicator.getSpecialities(onSuccess: handleReceivedRoles) { _ in }
+    }
+
+    private func handleReceivedRoles(_ dto: EnumListDTO?) {
+        guard let data = dto?.data else { return }
+
+        speciality = data
+        observers.forEach { $0?.onDataSourceUpdated(speciality) }
+    }
+}
+
+extension ConsultationsViewModel: Observable {
+    func subscribe(_ observer: ObserverDelegate) {
+        if let observer = observer as? DataSourceObserverDelegate {
+            observers.append(observer)
+        }
+    }
+
+    func unsubscribe(_ observer: ObserverDelegate) {
+        observers = observers.filter { $0?.observerId != observer.observerId }
+    }
+}
