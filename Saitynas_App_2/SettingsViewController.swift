c14ac67d301033ce1d7080954b757c2de8a362ae
Vytenis Petrauskas <visak.pet0@gmail.com>
2022-03-22 23:27:51 +0200

    Add activity switch

--- Saitynas_App_2/SettingsViewController.swift
+++ Saitynas_App_2/SettingsViewController.swift
@@ -2,6 +2,9 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var activityToggleView: ShadowView!
+    @IBOutlet weak var activityStatusLabel: UILabel!
+
     @IBOutlet weak var emailLabel: UILabel!
 
     private weak var authenticationManager: AuthenticationManager!
@@ -16,9 +19,15 @@ class SettingsViewController: UIViewController {
         let c = DIContainer.shared
 
         authenticationManager = c.authenticationManager
+
+        activityToggleView.isHidden = c.jwtUser.role != Role.specialist
         emailLabel.text = c.jwtUser.email ?? "Failed to get email!"
     }
-    
+
+    @IBAction func activitySwitchToggled(_ sender: UISwitch) {
+        activityStatusLabel.text = sender.isOn ? "Available" : "Busy"
+    }
+
     @IBAction func logoutButtonPressed(_ sender: UIButton) {
         authenticationManager.logout()
     }
