bc7f477e6b1a11f90756c2fc3df0ab1c2376f09a
Vytenis Petrauskas <visak.pet0@gmail.com>
2022-04-23 19:36:25 +0300

    Extract view with keyboard base

--- Saitynas_App_2/AccessControllerBase.swift
+++ Saitynas_App_2/AccessControllerBase.swift
@@ -1,22 +1,14 @@
 import UIKit
 
-class AccessControllerBase: UIViewController {
+class AccessControllerBase: ViewWithKeyboardBase {
 
     let errorColor = UIColor.fromColorCode(.errorRed)
     let inputBorderColor = UIColor.fromColorCode(.inputFieldBorder)
 
-    private weak var bottomConstraint: NSLayoutConstraint?
+    override func viewDidLoad(_ constraint: NSLayoutConstraint) {
+        super.viewDidLoad(constraint)
 
-    private var defaultConstraintDistance: CGFloat!
-
-    private let distanceFromKeyboard: CGFloat = 16
-
-    func viewDidLoad(_ constraint: NSLayoutConstraint) {
-        super.viewDidLoad()
-
-        bottomConstraint = constraint
-        defaultConstraintDistance = constraint.constant
-        addKeyboardObservers()
+        super.addKeyboardObservers()
     }
 
     override func viewWillDisappear(_ animated: Bool) {
@@ -46,64 +38,5 @@ class AccessControllerBase: UIViewController {
         )
 
         present(alert, animated: true, completion: nil)
-    }
-}
-
-// MARK: - Keyboard Observers
-extension AccessControllerBase {
-    private func addKeyboardObservers() {
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(keyboardWillShow),
-            name: UIResponder.keyboardWillShowNotification,
-            object: nil
-        )
-
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(keyboardWillHide),
-            name: UIResponder.keyboardWillHideNotification,
-            object: nil
-        )
-    }
-
-    @objc private func keyboardWillShow(sender: NSNotification) {
-        let info = sender.userInfo
-
-        guard
-            let keyboardSize = getKeyboardSize(info),
-            let duration = getKeyboardAnimationDuration(info)
-        else { return }
-
-        let distance = keyboardSize - bottomLayoutGuide.length + distanceFromKeyboard
-        bottomConstraint?.constant = distance
-
-        UIView.animate(
-            withDuration: duration,
-            animations: view.layoutIfNeeded
-        )
-    }
-
-    @objc private func keyboardWillHide(sender: NSNotification) {
-        guard let duration = getKeyboardAnimationDuration(sender.userInfo) else {
-            return
-        }
-
-        bottomConstraint?.constant = defaultConstraintDistance
-
-        UIView.animate(
-            withDuration: duration,
-            animations: view.layoutIfNeeded
-        )
-    }
-
-    private func getKeyboardSize(_ userInfo: [AnyHashable: Any]?) -> CGFloat? {
-        let frame = userInfo?[UIResponder.keyboardFrameEndUserInfoKey]
-        return (frame as? NSValue)?.cgRectValue.height
-    }
-
-    private func getKeyboardAnimationDuration(_ userInfo: [AnyHashable: Any]?) -> TimeInterval? {
-        let duration = userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey]
-        return (duration as? NSNumber)?.doubleValue
     }
 }
